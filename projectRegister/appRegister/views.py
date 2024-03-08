510f30dc15642ddd4e8b2eeeb0d4f3d3358645ff
Gomes <gdemonst@gmail.com>
2024-03-07 21:12:47 -0300

    feat: db and views config

--- projectRegister/appRegister/views.py
+++ projectRegister/appRegister/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
 
 def home(request):
-    return render()
+    return render(request, 'usuarios/home.html')
+
+def usuarios(request):
+    if request.method == 'POST':
+        novoUsuario = Usuario()
+        novoUsuario.nome_usuario = request.POST.get('nomeUsuario')
+        novoUsuario.email_usuario = request.POST.get('emailUsuario')
+        
+        if Usuario.objects.filter(email_usuario=novoUsuario.email_usuario).exists():
+            message = "O valor já existe no banco de dados."
+        else:
+            novoUsuario.save()
+            message = "Usuário criado com sucesso!"
+        
+        usuarios = {
+            'usuarios': Usuario.objects.all(),
+            'message': message
+        }
+        return render(request, 'sua_template.html', usuarios)
+    else:
+        # Certifique-se de retornar um HttpResponse em todos os casos possíveis.
+        # Aqui, estamos retornando uma página vazia se o método não for POST.
+        return HttpResponse()
--- projectRegister/projectRegister/urls.py
+++ projectRegister/projectRegister/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from appRegister import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('usuarios/', views.usuarios, name="listaUsuarios")
 ]
